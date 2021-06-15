2a6710e5b9d827da8dd1a0758b56d381f5d85330
Bhavinpv <84716754+Bhavinpv@users.noreply.github.com>
2021-06-15 15:15:27 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -80,34 +80,28 @@ int max(int arr[], int n) {
    for(int i=0; i<count_1;i++) {
    arr[i] = 2;
    }
-   
-   for( int i=3 ; i<=sqrt(n) ; i+2) {
+   count=count_1;
+   for( int i=3 ; i< pow(n,0.5) ; i+2) {
     int count_2=0;
     while(n % i ==0) {
     count_2 +=1;
     n= n/i ;
     }
-    int count;
-    count= sizeof(arr)/sizeof(int);
+    
+    count += count_2;
     
      for(int j=0; j<count_2;j++) {
-   arr[count+j] = i;
+   arr[count- count_2+j] = i;
     }
      
    }
-   count= sizeof(arr)/sizeof(int);
+   
      if (n > 2) {
         arr[count] = n;
         }
 
-   count= sizeof(arr)/sizeof(int);
+ 
     return count;
    }
     
      
-  
-  
-  
-   
-   
-  
