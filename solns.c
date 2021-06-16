78c0c916f2d0c305f4d5ce313fa30b59c39fb0ea
Bhavinpv <84716754+Bhavinpv@users.noreply.github.com>
2021-06-16 13:07:12 +0530

    Update solns.c

--- solns.c
+++ solns.c
@@ -72,8 +72,7 @@ int max(int arr[], int n) {
  int factors(int n , int arr[]) {
   int count=0;
    while (n !=1) {
-
-     for (int i =2; i<=n ; i++){
+        for (int i =2; i<=n ; i++){
            if (n%i ==0) {
                arr[count] = i;
                 count++;
